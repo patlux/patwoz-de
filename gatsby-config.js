39a678d0c853a89861c1b29e660bed5100fbbf8d
Patrick Wozniak <email@patwoz.de>
2019-04-05 07:47:57 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My personal website`,
+    description: ``,
+    author: `@patwoz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -22,7 +22,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -31,4 +31,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,53 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+import pkg from '../../package.json';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO title="Home" keywords={pkg.keywords} />
+    <h1>Hi, I'm Patrick Wozniak</h1>
+    <p>
+      I live in Nuremberg with my lovely girlfriend. I build amazing stuff at{' '}
+      <a rel="noopener noreferrer" target="_blank" href="https://www.cegeka.com/">
+        Cegeka Deutschland GmbH
+      </a>
+      . When it comes to building things, I like to move rapidly. I utilize technologies such as
+      <a rel="noopener noreferrer" target="_blank" href="https://reactjs.org/">
+        React
+      </a>{' '}
+      (
+      <a rel="noopener noreferrer" target="_blank" href="https://facebook.github.io/react-native/">
+        Native
+      </a>
+      ),{' '}
+      <a rel="noopener noreferrer" target="_blank" href="https://expo.io/">
+        Expo
+      </a>
+      ,{' '}
+      <a rel="noopener noreferrer" target="_blank" href="https://zeit.co/now">
+        Now
+      </a>
+      .
+    </p>
+    <p>You can find me on the internet at:</p>
+    <ul>
+      <li>
+        <a rel="noopener noreferrer" target="_blank" href="mailto:email@patwoz.de">
+          email@patwoz.de
+        </a>
+      </li>
+      <li>
+        <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/de_patwoz">
+          @de_patwoz
+        </a>
+      </li>
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
