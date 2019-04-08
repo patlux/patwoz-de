500eb636c60182c48d53de465c4c1bb8664a3001
Patrick Wozniak <email@patwoz.de>
2019-04-08 20:52:31 +0200

    adding icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `patwoz`,
+        short_name: `patwoz`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
