061b3469b67e1586272e2f49a294009a69bed4b8
Patrick Wozniak <email@patwoz.de>
2019-06-27 23:18:30 +0200

    initial project with next (replacing gatsby)

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const IndexPage = ({ listOfAddresses }) => (
+  <>
+    <h1>Hi, I'm Patrick Wozniak</h1>
+    <p>
+      I live in Nuremberg with my lovely girlfriend. I build amazing stuff at{' '}
+      <a href="https://www.cegeka.com/">Cegeka Deutschland GmbH</a>. When it
+      comes to building things, I like to move rapidly. I utilize technologies
+      such as <a href="https://reactjs.org/">React</a> (
+      <a href="https://facebook.github.io/react-native/">Native</a>
+      ), <a href="https://expo.io/snacks/@patwoz">Expo</a>,{' '}
+      <a href="https://zeit.co/now">Now</a>,{' '}
+      <a href="https://about.gitlab.com/product/continuous-integration/">
+        Gitlab Ci
+      </a>
+      .
+    </p>
+    <p>You can find me on the internet at:</p>
+    <ul>
+      {listOfAddresses.map(Address => (
+        <li key={Address.link}>
+          <a href={Address.link}>{Address.description}</a>
+        </li>
+      ))}
+    </ul>
+  </>
+);
+
+IndexPage.defaultProps = {
+  listOfAddresses: [
+    {
+      link: 'mailto:email@patwoz.de',
+      description: 'email@patwoz.de',
+    },
+    {
+      link: 'https://twitter.com/de_patwoz',
+      description: 'Twitter: @de_patwoz',
+    },
+    {
+      link: 'https://github.com/patlux',
+      description: 'Github: patlux',
+    },
+    {
+      link: 'https://stackoverflow.com/users/6300994/patrick-wozniak',
+      description: ' Stack Overflow',
+    },
+  ],
+};
+
+export default IndexPage;
