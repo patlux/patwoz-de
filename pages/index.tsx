909865a7fad44397f50b2570a15f2540a6ae3f86
Patrick Wozniak <email@patwoz.de>
2019-07-10 22:00:39 +0200

    [add] meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 import '../styles/index.css';
 import { Contact } from '../types';
+import Meta from '../components/Meta';
 
 export interface Props {
   listOfAddresses?: Contact[];
@@ -30,67 +32,73 @@ const INITIAL_LIST_OF_ADDRESSES: Contact[] = [
 const IndexPage: NextPage<Props> = ({
   listOfAddresses = INITIAL_LIST_OF_ADDRESSES,
 }) => (
-  <main>
-    <span>Hi, I'm</span>
-    <h1>
-      Patrick Wozniak{' '}
-      <a
-        className="link-inherit"
-        href="static/beauty-512.jpg"
-        title="Open image of Patrick Wozniak"
-      >
-        <img
-          src="static/beauty-34.jpg"
-          alt="Face photo of Patrick Wozniak"
-          width="34"
-          height="34"
-        ></img>
-      </a>
-    </h1>
-    <h3>Software Engineer in Nuremberg</h3>
-    <ul className="contact-list">
-      {listOfAddresses.map(Address => (
-        <li key={Address.link}>
-          <a href={Address.link}>{Address.description}</a>
-        </li>
-      ))}
-    </ul>
-    <p>
-      I live in Nuremberg with my lovely girlfriend. I build amazing stuff at{' '}
-      <a href="https://www.cegeka.com/" target="_blank">
-        Cegeka Deutschland GmbH
-      </a>
-      . When it comes to building things, I like to move rapidly. I utilize
-      technologies such as{' '}
-      <a href="https://reactjs.org/" target="_blank">
-        React
-      </a>{' '}
-      (
-      <a href="https://facebook.github.io/react-native/" target="_blank">
-        Native
-      </a>
-      ),{' '}
-      <a href="https://nextjs.org/" target="_blank">
-        Nextjs
-      </a>
-      ,{' '}
-      <a href="https://zeit.co/now" target="_blank">
-        Now
-      </a>
-      ,{' '}
-      <a href="https://expo.io/snacks/@patwoz" target="_blank">
-        Expo
-      </a>
-      ,{' '}
-      <a
-        href="https://about.gitlab.com/product/continuous-integration/"
-        target="_blank"
-      >
-        Gitlab Ci
-      </a>
-      .
-    </p>
-  </main>
+  <>
+    <Meta />
+    <Head>
+      <title>Patrick Wozniak</title>
+    </Head>
+    <main>
+      <span>Hi, I'm</span>
+      <h1>
+        Patrick Wozniak{' '}
+        <a
+          className="link-inherit"
+          href="static/beauty-512.jpg"
+          title="Open image of Patrick Wozniak"
+        >
+          <img
+            src="static/beauty-34.jpg"
+            alt="Face photo of Patrick Wozniak"
+            width="34"
+            height="34"
+          ></img>
+        </a>
+      </h1>
+      <h3>Software Engineer in Nuremberg</h3>
+      <ul className="contact-list">
+        {listOfAddresses.map(Address => (
+          <li key={Address.link}>
+            <a href={Address.link}>{Address.description}</a>
+          </li>
+        ))}
+      </ul>
+      <p>
+        I live in Nuremberg with my lovely girlfriend. I build amazing stuff at{' '}
+        <a href="https://www.cegeka.com/" target="_blank">
+          Cegeka Deutschland GmbH
+        </a>
+        . When it comes to building things, I like to move rapidly. I utilize
+        technologies such as{' '}
+        <a href="https://reactjs.org/" target="_blank">
+          React
+        </a>{' '}
+        (
+        <a href="https://facebook.github.io/react-native/" target="_blank">
+          Native
+        </a>
+        ),{' '}
+        <a href="https://nextjs.org/" target="_blank">
+          Nextjs
+        </a>
+        ,{' '}
+        <a href="https://zeit.co/now" target="_blank">
+          Now
+        </a>
+        ,{' '}
+        <a href="https://expo.io/snacks/@patwoz" target="_blank">
+          Expo
+        </a>
+        ,{' '}
+        <a
+          href="https://about.gitlab.com/product/continuous-integration/"
+          target="_blank"
+        >
+          Gitlab Ci
+        </a>
+        .
+      </p>
+    </main>
+  </>
 );
 
 export default IndexPage;
