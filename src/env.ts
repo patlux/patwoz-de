69c90340102ec389134c1e62b59cdb12459c5956
Patrick Wozniak <email@patwoz.de>
2020-10-28 22:01:17 +0100

    fix(tracking): run tracking scripts only in production

--- src/env.ts
+++ src/env.ts
@@ -6,3 +6,4 @@ export function stringify(str?: string) {
 }
 
 export const TRACKING_ID = stringify(process.env.TRACKING_ID);
+export const NODE_ENV = stringify(process.env.NODE_ENV);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import { TRACKING_ID } from '../env';
+import { NODE_ENV, TRACKING_ID } from '../env';
 
 export default class extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {TRACKING_ID && (
+          {NODE_ENV === 'production' && TRACKING_ID && (
             <script
               async
               defer
