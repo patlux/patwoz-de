cc61f2939cba33e0ad87b5c03a4436b7695d04ab
Patrick Wozniak <email@patwoz.de>
2021-07-23 23:29:02 +0200

    clean

--- src/env.ts
+++
@@ -1,9 +0,0 @@
-export function stringify(str?: string) {
-  if (typeof str !== 'string' || str.length === 0) {
-    return null;
-  }
-  return str;
-}
-
-export const TRACKING_ID = stringify(process.env.TRACKING_ID);
-export const NODE_ENV = stringify(process.env.NODE_ENV);
