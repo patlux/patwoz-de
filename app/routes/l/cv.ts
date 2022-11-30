d244b51fd68adc76e1eca4b90832d9e1ca8f3d12
Patrick Wozniak <email@patwoz.de>
2022-11-30 13:24:22 +0100

    style(code): format code with prettier

--- app/routes/l/cv.ts
+++ app/routes/l/cv.ts
@@ -2,5 +2,7 @@ import type { LoaderFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 
 export const loader: LoaderFunction = async () => {
-  return redirect('https://www.freelance.de/Freiberufler/215868-Senior-TypeScript-Developer-ReactJS-React-Native/highlight=react');
+  return redirect(
+    'https://www.freelance.de/Freiberufler/215868-Senior-TypeScript-Developer-ReactJS-React-Native/highlight=react'
+  );
 };
