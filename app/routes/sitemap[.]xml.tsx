53de324732448bab5132d059daaf880fa7419871
Patrick Wozniak <email@patwoz.de>
2022-11-30 14:27:45 +0100

    fix(sitemap): only change lastmod when running build

--- app/routes/sitemap[.]xml.tsx
+++ app/routes/sitemap[.]xml.tsx
@@ -1,24 +1,24 @@
 const URLS = ['https://patwoz.dev/', 'https://patwoz.dev/imprint'];
 
-export const loader = () => {
-  const urlsContent = URLS.map((url) => {
-    return `
+const urlsContent = URLS.map((url) => {
+  return `
       <url>
         <loc>${url}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>1.0</priority>
       </url>`;
-  }).join('\n');
+}).join('\n');
 
-  const content = `
+const content = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${urlsContent}
     </urlset>
     `.trim();
 
-  // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
+
+export const loader = () => {
   return new Response(content, {
     status: 200,
     headers: {
