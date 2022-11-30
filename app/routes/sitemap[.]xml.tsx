fa974f9e70e8e6c91b15bcb6cb1764950e644d9a
Patrick Wozniak <email@patwoz.de>
2022-11-30 13:53:18 +0100

    fix(sitemapxml): setup sitemap.xml

--- app/routes/sitemap[.]xml.tsx
+++ app/routes/sitemap[.]xml.tsx
@@ -1,13 +1,26 @@
+const URLS = [
+  'https://patwoz.dev/',
+  'https://patwoz.dev/imprint'
+]
+
 export const loader = () => {
+  const urlsContent = URLS.map(url => {
+    return `
+      <url>
+        <loc>${url}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+      </url>`
+  }).join('\n')
+
   const content = `
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-    <loc>https://patwoz.dev/</loc>
-    <lastmod>2022-01-08T00:15:16+01:00</lastmod>
-    <priority>1.0</priority>
-    </url>
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urlsContent}
     </urlset>
-    `;
+    `.trim();
+
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
   return new Response(content, {
     status: 200,
