0f0958d6eda9e21715e3548334009a6c77b6bf2d
Patrick Wozniak <email@patwoz.de>
2022-11-30 12:02:30 +0100

    fix(remix): upgrade remix to latest version

--- app/routes/l/linkedin.ts
+++ app/routes/l/linkedin.ts
@@ -1,5 +1,5 @@
-import type { LoaderFunction } from 'remix';
-import { redirect } from 'remix';
+import type { LoaderFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
 
 export const loader: LoaderFunction = async () => {
   return redirect('https://de.linkedin.com/in/patrick-wozniak-696453123');
