4f4aacf648296495a91c7a71e8a63ad5994b5732
Patrick Wozniak <email@patwoz.de>
2022-12-28 22:42:45 +0100

    feat(bun): adding remix-bun

--- server.ts
+++ server.ts
@@ -1,69 +1,23 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import type { ServeOptions } from 'bun';
-import type { RequestHandler } from '@remix-run/node';
-import type { ServerBuild } from '@remix-run/server-runtime';
-import { createRequestHandler } from '@remix-run/server-runtime';
+import { createRequestHandler } from 'remix-bun';
+import { withStaticDir } from './server-utils';
 
-const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-
-setInterval(() => {
-  Bun.gc(true);
-}, 9000);
-
-const getBuild = async (): Promise<ServerBuild> => import('./build') as any as ServerBuild;
-
-let prodRequestHandler: RequestHandler;
-
-async function handler(request: Request): Promise<Response> {
-  let requestHandler: RequestHandler;
-
-  if (mode === 'production') {
-    if (!prodRequestHandler) {
-      prodRequestHandler = createRequestHandler(await getBuild(), mode);
-    }
-    requestHandler = prodRequestHandler;
-  } else {
-    const url = new URL(request.url);
-    console.log(`${request.method.toUpperCase()} ${url.pathname}${url.search}`);
-    requestHandler = createRequestHandler(await getBuild(), mode);
-  }
-
-  return requestHandler(request);
-}
-
-const withStaticDir =
-  (staticDir: string) => (requestHandler: RequestHandler) => (request: Request) => {
-    const file = tryServeStaticFile(staticDir, request);
-    if (file) return file;
-    return requestHandler(request);
-  };
-
-function tryServeStaticFile(staticDir: string, request: Request): Response | undefined {
-  const url = new URL(request.url);
-
-  if (url.pathname.length < 2) return undefined;
-
-  const filePath = path.join(staticDir, url.pathname);
-
-  if (fs.existsSync(filePath)) {
-    const file = Bun.file(filePath);
-    return new Response(file, {
-      headers: {
-        'Content-Type': file.type,
-        'Cache-Control': 'public, max-age=31536000',
-      },
-    });
-  }
-
-  return undefined;
-}
-
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+setInterval(() => Bun.gc(true), 9000);
 
 const bunServeOptions: ServeOptions = {
-  port,
-  fetch: withStaticDir('public')(handler),
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  fetch: withStaticDir('public')(
+    process.env.NODE_ENV === 'production'
+      ? createRequestHandler({
+          build: require('./build'),
+          mode: 'production',
+        })
+      : async (request: Request) => {
+          const build = require('./build');
+          const requestHandler = createRequestHandler({ build, mode: 'development' });
+          return requestHandler(request);
+        }
+  ),
 };
 
 export default bunServeOptions;
