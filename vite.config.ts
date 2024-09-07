2729ab6e56e11177c167a8fdcadd73f6844d56fd
Patrick Wozniak <email@patwoz.de>
2024-09-07 10:18:49 +0200

    upgrade some deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-console.log(`PORT=${process.env.PORT}`)
-
 export default defineConfig({
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : undefined,
