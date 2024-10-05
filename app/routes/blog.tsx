df164c097fab92bb8be5523daba82bdcd19e67b7
Patrick Wozniak <email@patwoz.de>
2024-10-05 12:02:20 +0200

    adding css for line numbers

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,17 +1,6 @@
-import type { MetaFunction } from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
 import { BaseLayout } from '~/components/BaseLayout'
 import { Footer } from '~/components/Footer'
-
-export const meta: MetaFunction = ({ data }) => {
-  console.log({ data })
-  return []
-}
-
-// export const loader = ({ request }: LoaderFunctionArgs) => {
-//   console.log(request.url)
-//   return null
-// }
 
 export default function Component() {
   return (
