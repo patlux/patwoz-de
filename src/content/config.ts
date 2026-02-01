7c6ca94bcf89bd4e94c5957494aef7a5e48eeb6f
Patrick Wozniak <email@patwoz.de>
2026-02-01 23:44:51 +0100

    german translation

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,4 +11,15 @@ const blog = defineCollection({
   }),
 })
 
-export const collections = { blog }
+const blogDe = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.coerce.date(),
+    image: z.string().optional(),
+    featured: z.boolean().optional().default(false),
+  }),
+})
+
+export const collections = { blog, 'blog-de': blogDe }
